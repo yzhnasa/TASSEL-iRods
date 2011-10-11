ab830462dc68e46a3fdeed7146ff0e7b9331526a
tcasstevens <tcasstevensusers.sourceforge.net>
2011-10-11 17:38:52 -0400

    Initial

--- src/net/maizegenetics/tassel/TASSELMainFrame.java
+++ src/net/maizegenetics/tassel/TASSELMainFrame.java
@@ -78,7 +78,7 @@ public class TASSELMainFrame extends JFrame {
 
     private static final Logger myLogger = Logger.getLogger(TASSELMainFrame.class);
     public static final String version = "4.0.0";
-    public static final String versionDate = "September 8, 2011";
+    public static final String versionDate = "October 11, 2011";
     DataTreePanel theDataTreePanel;
     DataControlPanel theDataControlPanel;
     AnalysisControlPanel theAnalysisControlPanel;
