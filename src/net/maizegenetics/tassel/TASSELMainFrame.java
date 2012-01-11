13e45b6527a6aa677f163e31e54fad21d05a868d
tcasstevens <tcasstevensusers.sourceforge.net>
2012-01-11 15:10:48 -0500

    New Build Date

--- src/net/maizegenetics/tassel/TASSELMainFrame.java
+++ src/net/maizegenetics/tassel/TASSELMainFrame.java
@@ -77,8 +77,8 @@ import org.apache.log4j.Logger;
 public class TASSELMainFrame extends JFrame {
 
     private static final Logger myLogger = Logger.getLogger(TASSELMainFrame.class);
-    public static final String version = "4.0.3";
-    public static final String versionDate = "January 5, 2012";
+    public static final String version = "4.0.4";
+    public static final String versionDate = "January 12, 2012";
     DataTreePanel theDataTreePanel;
     DataControlPanel theDataControlPanel;
     AnalysisControlPanel theAnalysisControlPanel;
