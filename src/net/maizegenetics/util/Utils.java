102af888509818ad572c3e399b57986a3fb2492a
tcasstevens <tcasstevens@users.sourceforge.net>
2012-08-02 17:10:05 -0400

    Add logging to Tassel Pipeline to show actual max heap size.

--- src/net/maizegenetics/util/Utils.java
+++ src/net/maizegenetics/util/Utils.java
@@ -328,4 +328,14 @@ public final class Utils {
         }
         return result;
     }
+
+    /**
+     * Returns max heap size in MB.
+     * 
+     * @return max heap size 
+     */
+    public static long getMaxHeapSizeMB() {
+        return Runtime.getRuntime().maxMemory() / 1048576l;
+    }
+    
 }
