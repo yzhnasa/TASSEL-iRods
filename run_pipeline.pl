3e198b8890b7559dffa53d89fe232d3ce74f83ba
tcasstevens <tcasstevens@users.sourceforge.net>
2012-08-02 22:46:17 -0400

    Corrected perl scripts

--- run_pipeline.pl
+++ run_pipeline.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl -w
 
 use strict;
-my $top = '.';
+use File::Basename;
+
+my $top = dirname(__FILE__);
+$top //= '.';
+
 my $libdir = "$top/lib";
 opendir (DIR, "$libdir") || die "Could not open $libdir\n";
 my @list = readdir(DIR);
@@ -14,8 +18,30 @@ foreach my $fn(@list){
 }
 push(@fl, "$top/dist/sTASSEL.jar");
 my $CP = join(":", @fl);
-print $CP;
-print "\n";
-my @args = @ARGV;
+print $CP . "\n";
 
-system "java -classpath '$CP' -Xms512m -Xmx1536m net.maizegenetics.pipeline.TasselPipeline @args";
+# Scan @ARGV for Java memory arguments, and put rest in @args
+my $java_mem_min_default = "-Xms512m";
+my $java_mem_max_default = "-Xmx1536m";
+my $java_mem_min = "";
+my $java_mem_max = "";
+my @args;
+for (my $i=0; $i<=$#ARGV; $i++){
+   if ($ARGV[$i] =~ m/Xms/) {
+      $java_mem_min .= "$ARGV[$i]";
+   }
+   elsif ($ARGV[$i] =~ m/Xmx/) {
+      $java_mem_max .= "$ARGV[$i]";
+   }
+   else{
+      push(@args, $ARGV[$i]);
+   }
+}
+
+if ($java_mem_min eq "") { $java_mem_min = $java_mem_min_default; }
+if ($java_mem_max eq "") { $java_mem_max = $java_mem_max_default; }
+
+print "Memory Settings: $java_mem_min $java_mem_max\n";
+print "Tassel Pipeline Arguments: " . "@args\n";
+
+system "java -classpath '$CP' $java_mem_min $java_mem_max net.maizegenetics.pipeline.TasselPipeline @args";
