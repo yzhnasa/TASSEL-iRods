9ab19596ac8e621a7c5545befc22b9dcd78a5ba4
tcasstevens <tcasstevens@users.sourceforge.net>
2012-08-04 03:53:41 -0400

    Add UNKNOWN constant.

--- src/net/maizegenetics/pal/alignment/Locus.java
+++ src/net/maizegenetics/pal/alignment/Locus.java
@@ -17,6 +17,8 @@ import java.util.Map;
 public class Locus implements Serializable {
 
     private static final long serialVersionUID = -5197800047652332969L;
+    public static Locus UNKNOWN = new Locus("Unknown", "0", 0, 0, null, null);
+    
     private final String myName;
     private final String myChromosome;
     private final int myStart;
