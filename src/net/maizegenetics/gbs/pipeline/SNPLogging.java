24f56f52c38df5f3bdf5b0bd0aa6605fc8412cbf
Terry Casstevens <tmc46@cornell.edu>
2012-11-09 17:32:05 -0500

    Updated to send log messages to console if no file defined.

--- src/net/maizegenetics/gbs/pipeline/SNPLogging.java
+++ src/net/maizegenetics/gbs/pipeline/SNPLogging.java
@@ -7,6 +7,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import net.maizegenetics.pal.alignment.Alignment;
 import net.maizegenetics.util.Utils;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -14,24 +15,29 @@ import net.maizegenetics.util.Utils;
  */
 public class SNPLogging {
 
+    private final Logger myLogger = Logger.getLogger(SNPLogging.class);
     private static final String HEADER = "Chr\tPosition\tAlleles\tTagLocusStart\tStrand\tPlugin\tTest\tStatus\tValue\tCuttoff\n";
     private static final String DELIMITER = "\t";
     private final BufferedWriter myWriter;
 
     public SNPLogging(String filename) {
 
-        boolean exists = false;
-        File file = new File(filename);
-        if (file.exists()) {
-            exists = true;
-        }
+        if ((filename == null) || (filename.length() == 0)) {
+            myWriter = null;
+        } else {
+            boolean exists = false;
+            File file = new File(filename);
+            if (file.exists()) {
+                exists = true;
+            }
 
-        myWriter = Utils.getBufferedWriter(filename, true);
-        if (!exists) {
-            try {
-                myWriter.append(HEADER);
-            } catch (Exception e) {
-                e.printStackTrace();
+            myWriter = Utils.getBufferedWriter(filename, true);
+            if (!exists) {
+                try {
+                    myWriter.append(HEADER);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -41,39 +47,48 @@ public class SNPLogging {
         try {
             myWriter.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            // do nothing;
         }
     }
-    
+
     public void writeEntry(Alignment a, int site, String tagLocusStart, String strand, Class plugin, String test, String status, String value, String cuttoff) {
         String alleles = a.getMajorAlleleAsString(site) + "/" + a.getMinorAlleleAsString(site);
         writeEntry(a.getLocusName(site), a.getPositionInLocus(site), alleles, tagLocusStart, strand, plugin, test, status, value, cuttoff);
     }
 
     public void writeEntry(String chr, int position, String alleles, String tagLocusStart, String strand, Class plugin, String test, String status, String value, String cuttoff) {
-        try {
-            myWriter.append(chr);
-            myWriter.append(DELIMITER);
-            myWriter.append(String.valueOf(position));
-            myWriter.append(DELIMITER);
-            myWriter.append(alleles);
-            myWriter.append(DELIMITER);
-            myWriter.append(tagLocusStart);
-            myWriter.append(DELIMITER);
-            myWriter.append(strand);
-            myWriter.append(DELIMITER);
-            myWriter.append(plugin.getSimpleName());
-            myWriter.append(DELIMITER);
-            myWriter.append(test);
-            myWriter.append(DELIMITER);
-            myWriter.append(status);
-            myWriter.append(DELIMITER);
-            myWriter.append(value);
-            myWriter.append(DELIMITER);
-            myWriter.append(cuttoff);
-            myWriter.append("\n");
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(chr);
+        builder.append(DELIMITER);
+        builder.append(String.valueOf(position));
+        builder.append(DELIMITER);
+        builder.append(alleles);
+        builder.append(DELIMITER);
+        builder.append(tagLocusStart);
+        builder.append(DELIMITER);
+        builder.append(strand);
+        builder.append(DELIMITER);
+        builder.append(plugin.getSimpleName());
+        builder.append(DELIMITER);
+        builder.append(test);
+        builder.append(DELIMITER);
+        builder.append(status);
+        builder.append(DELIMITER);
+        builder.append(value);
+        builder.append(DELIMITER);
+        builder.append(cuttoff);
+        builder.append("\n");
+
+        if (myWriter == null) {
+            myLogger.info(builder.toString());
+        } else {
+            try {
+                myWriter.append(builder.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
     }
 }
