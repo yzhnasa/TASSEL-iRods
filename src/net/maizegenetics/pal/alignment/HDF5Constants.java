abe189b88c8162339ae57194a40206e7445acc58
Terry Casstevens <tmc46@cornell.edu>
2012-11-30 12:17:17 -0500

    New HDF5Constants Class

---
+++ src/net/maizegenetics/pal/alignment/HDF5Constants.java
@@ -0,0 +1,20 @@
+/*
+ * HDF5Constants
+ */
+package net.maizegenetics.pal.alignment;
+
+/**
+ *
+ * @author terry
+ */
+public final class HDF5Constants {
+    
+    public static String ROOT = "/";
+    public static String NUM_WORDS_PATH = ROOT;
+    public static String NUM_WORDS = "numWords";
+    
+    private HDF5Constants() {
+        // do not instantiate
+    }
+    
+}
