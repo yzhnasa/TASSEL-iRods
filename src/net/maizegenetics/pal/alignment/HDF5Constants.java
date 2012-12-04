2722d24b0089fbf6025ab7a1f51f7f5b84e9d80b
Terry Casstevens <tmc46@cornell.edu>
2012-12-03 23:25:37 -0500

    Added more constants

--- src/net/maizegenetics/pal/alignment/HDF5Constants.java
+++ src/net/maizegenetics/pal/alignment/HDF5Constants.java
@@ -16,17 +16,18 @@ public final class HDF5Constants {
     public static final String ALLELES = "Alleles";
     public static final String TBIT = "TBit";
     public static final String SBIT = "SBit";
-    public static final String LOCI = "Loci";
-    public static final String LOCUS_OFFSETS = "LocusOffsets";
+    public static final String LOCI = "SeqRegion";
+    public static final String LOCUS_OFFSETS = "SeqRegionOffsets";
+    public static final String SNP_IDS = "SnpIds";
     
     // Attributes
-    public static final String NUM_WORDS_PATH = ROOT;
+    public static final String DEFAULT_ATTRIBUTES_PATH = ROOT;
     public static final String NUM_SBIT_WORDS = "numSBitWords";
     public static final String NUM_TBIT_WORDS = "numTBitWords";
-    public static final String NUM_TAXA_PATH = ROOT;
     public static final String NUM_TAXA = "numTaxa";
-    public static final String NUM_SITES_PATH = ROOT;
     public static final String NUM_SITES = "numSites";
+    public static final String MAX_NUM_ALLELES = "maxNumAlleles";
+    public static final String RETAIN_RARE_ALLELES = "retainRareAlleles";
     
     private HDF5Constants() {
         // do not instantiate
