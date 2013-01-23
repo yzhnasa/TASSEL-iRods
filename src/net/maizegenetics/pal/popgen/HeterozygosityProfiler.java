2e88c0eb4146ab9f6b13f14d6bd3e70e7dd384b7
edbuckler <edbuckler@edimac11.maize.cornell.edu>
2013-01-23 17:21:04 -0500

    Outputs HapMap files with all the heterozygous regions set to Unknown

--- src/net/maizegenetics/pal/popgen/HeterozygosityProfiler.java
+++ src/net/maizegenetics/pal/popgen/HeterozygosityProfiler.java
@@ -11,8 +11,12 @@ import net.maizegenetics.gwas.imputation.EmissionProbability;
 import net.maizegenetics.gwas.imputation.TransitionProbability;
 import net.maizegenetics.gwas.imputation.ViterbiAlgorithm;
 import net.maizegenetics.pal.alignment.Alignment;
+import net.maizegenetics.pal.alignment.BitAlignmentHDF5;
+import net.maizegenetics.pal.alignment.ExportUtils;
 import net.maizegenetics.pal.alignment.ImportUtils;
+import net.maizegenetics.pal.alignment.MutableNucleotideAlignment;
 import net.maizegenetics.util.ProgressListener;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  *
@@ -20,25 +24,40 @@ import net.maizegenetics.util.ProgressListener;
  */
 public class HeterozygosityProfiler {
     private Alignment tA;
-    private float[][] blockHetProb=null;
+ //   private float[][] blockHetProb=null;
+    private MutableNucleotideAlignment mna;
+    private boolean report=false;
 
     public HeterozygosityProfiler(String unImpTargetFile, String exportFile) {
+        java.util.Properties props = new java.util.Properties();
+        props.setProperty("log4j.logger.net.maizegenetics", "INFO, stdout");
+        props.setProperty("log4j.appender.stdout",
+                "org.apache.log4j.ConsoleAppender");
+        props.setProperty("log4j.appender.stdout.layout",
+                "org.apache.log4j.TTCCLayout");
+        PropertyConfigurator.configure(props);
    
-        tA=ImportUtils.readFromHapmap(unImpTargetFile, false, (ProgressListener)null);
+        if(unImpTargetFile.endsWith(".h5")) {
+            tA=BitAlignmentHDF5.getInstance(unImpTargetFile);
+//        } else if(unImpTargetFile.endsWith(".h5")) {
+//            tA=ImportUtils(unImpTargetFile, false, (ProgressListener)null);
+        } else {
+            tA=ImportUtils.readFromHapmap(unImpTargetFile, false, (ProgressListener)null);
+        }
         tA.optimizeForTaxa(null);
         tA.optimizeForSites(null);
+        MutableNucleotideAlignment mna=MutableNucleotideAlignment.getInstance(this.tA);
         int blocks=tA.getAllelePresenceForAllSites(0, 0).getNumWords();
-        blockHetProb=new float[tA.getSequenceCount()][blocks];
+     //   blockHetProb=new float[tA.getSequenceCount()][blocks];
         
         int totalBlocks=0, totalhetBlocks=0;
         TransitionProbability tp=setTransitionProb(tA, blocks);
         double[][] hetExp=simulateHetInformation(-1, true);
         double[][] homoWithErrorExp=simulateHetInformation(0.002, false);
         for (int bt = 0; bt < tA.getSequenceCount(); bt+=1) {
-            int taxaImpCnt=0;
-            //DonorHypoth[][] regionHypth=new DonorHypoth[blocks][10];
             String name=tA.getIdGroup().getIdentifier(bt).getFullName();
-  //          System.out.printf("Imputing %d:%s ... %n", bt,name);
+  //          if(!name.contains("SEED")) continue;
+            System.out.printf("Het Masking %d:%s ... %n", bt,name);
             long time=System.currentTimeMillis();
             long[] mjT=tA.getAllelePresenceForAllSites(bt, 0).getBits();
             long[] mnT=tA.getAllelePresenceForAllSites(bt, 1).getBits();
@@ -58,36 +77,47 @@ public class HeterozygosityProfiler {
                 obs[b]=(byte)hetCnt[1][b];
                 obsRev[blocks-b-1]=obs[b];
             }
-            System.out.println("avgCovPerBlock:"+avgCovPerBlock);
+            if(report) System.out.println("avgCovPerBlock:"+avgCovPerBlock);
             EmissionProbability ep=setEmmisionProb(hetExp[avgCovPerBlock],homoWithErrorExp[avgCovPerBlock]);  //in the future needs to include coverage poisson
             double expectedPhet=0.8;
             double[] initialProb = new double[]{1 - expectedPhet, expectedPhet};
-        //    double[] initialProb = new double[]{1,0};
             ViterbiAlgorithm va = new ViterbiAlgorithm(obs, tp, ep, initialProb);
             va.calculate();
-            System.out.printf("Hets\t %d:%s \t %d \t %d %n", bt,name, sum, hsum);
+            if(report) System.out.printf("Hets\t %d:%s \t %d \t %d %n", bt,name, sum, hsum);
             byte[] states = va.getMostProbableStateSequence();
-            System.out.println(Arrays.toString(obs));
-            System.out.println(Arrays.toString(states));
+            if(report) System.out.println("OBS:"+Arrays.toString(obs));
+            if(report) System.out.println("FOR:"+Arrays.toString(states));
             va = new ViterbiAlgorithm(obsRev, tp, ep, initialProb);
             va.calculate();
             byte[] statesRev = va.getMostProbableStateSequence();
+            byte[] statesRevRev=new byte[statesRev.length];
             for (int b = 0; b < blocks; b++) {
-                states[b]=statesRev[blocks-b-1];
+                statesRevRev[b]=statesRev[blocks-b-1];
             }
-            System.out.println(Arrays.toString(states));
-            for (int focusBlock = 0; focusBlock < blocks; focusBlock++) {
-                blockHetProb[bt][focusBlock]=Float.NaN;
-//                int[] resultRange=getBlockWithMinMinorCount(mnT, focusBlock, minMinorCnt);
-//                total++;
-//                regionHypth[focusBlock]=getBestInbredDonors(bt, resultRange[0],resultRange[2], focusBlock);
-
-            }
+            if(report) System.out.println("REV:"+Arrays.toString(statesRevRev));
+            setAlignmentWithHetMask(bt,tA, mna, states,statesRevRev);
             
         }
         System.out.printf("Totals Blocks\t %d  HetBlocks\t %d \t %g %n", totalBlocks, 
                 totalhetBlocks, (double)totalhetBlocks/(double)totalBlocks);
+        ExportUtils.writeToHapmap(mna, false, exportFile, '\t', null);
         
+    }
+    
+    /**
+     * Takes a donor hypothesis and applies it to the output alignment 
+     * @param theDH
+     * @param mna 
+     */
+    private void setAlignmentWithHetMask(int taxon, Alignment tA, MutableNucleotideAlignment mna, byte[] fhetStates, byte[] rhetStates) {
+        for(int cs=0; cs<tA.getSiteCount(); cs++) {
+            int block=cs/64;
+            if((fhetStates[block]==1)||(rhetStates[block]==1)) {  //if heterozgyous region then set to missing
+                mna.setBase(taxon, cs, Alignment.UNKNOWN_DIPLOID_ALLELE);
+            } else {
+                mna.setBase(taxon, cs, tA.getBase(taxon, cs));
+            }
+        } //end of cs loop
     }
     
     private double[][] simulateHetInformation(double maf, boolean realMAF) {
@@ -97,12 +127,16 @@ public class HeterozygosityProfiler {
         int[] coverageCnt=new int[65];
         Random r=new Random();
         int mafcnt=0;
+        double[] mafReal=new double[tA.getSiteCount()];
+        for (int i = 0; i < mafReal.length; i++) {
+            mafReal[i]=tA.getMinorAlleleFrequency(i);
+        }
         for(int x=0; x<100000; x++) {
             byte[] mjA=new byte[64];
             byte[] mnA=new byte[64];
             for (int c = 0; c < x%256; c++) {
                 int site=r.nextInt(64);
-                if(realMAF) maf=tA.getMinorAlleleFrequency((mafcnt++)%tA.getSiteCount());
+                if(realMAF) maf=mafReal[(mafcnt++)%tA.getSiteCount()];
                 if(r.nextDouble()<maf) {mnA[site]++;}
                 else {mjA[site]++;}
             }
@@ -202,15 +236,19 @@ public class HeterozygosityProfiler {
      */
     public static void main(String[] args) {
       
-      String root="/Users/edbuckler/SolexaAnal/GBS/build20120110/imp/";
-//        String root="/Volumes/LaCie/build20120110/imp/";
+//      String root="/Users/edbuckler/SolexaAnal/GBS/build20120110/imp/";
+        String root="/Volumes/LaCie/build20120110/imp/";
 
         String donorFile=root+"NAMfounder20120110.imp.hmp.txt";
  //       String donorFile=root+"DTMAfounder20120110.imp.hmp.txt";
-        String unImpTargetFile=root+"ZeaSyn20120110.hmp.txt";
-        String impTargetFile=root+"ZeaSyn20120110.imp.hmp.txt";
+//        String unImpTargetFile=root+"ZeaSyn20120110.hmp.txt";
+//        String impTargetFile=root+"ZeaSyn20120110.hetmask.imp.hmp.txt";
+        
+      //  String unImpTargetFile="/Volumes/LaCie/build20120701/06_HapMap/rje22_BPEC_AllZea_GBS_Build_July_2012_RC-2_chr10.hmp.h5";
+        String unImpTargetFile="/Volumes/LaCie/build20120701/06_HapMap/AllTaxa_BPEC_AllZea_GBS_Build_July_2012_FINAL_chr10.hmp.txt.gz";
+        String impTargetFile="/Volumes/LaCie/build20120701/06_HapMap/AllTaxa_BPEC_AllZea_GBS_Build_July_2012_FINAL_chr10.hetmask.hmp.txt.gz";
 
-        boolean buildInput=true;
+        boolean buildInput=false;
         boolean filterTrue=true;
         if(buildInput) {KnownParentMinorWindowImputation.createSynthetic(donorFile, unImpTargetFile, 2000, 0.4, 0.5, 1000);}
 
