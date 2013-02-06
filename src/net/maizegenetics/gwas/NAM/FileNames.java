213dfa9e380a4370d654b0fa32262e07e0a77cc0
Peter Bradbury <pjb39@cornell.edu>
2013-02-06 12:53:40 -0500

    changed some default values

--- src/net/maizegenetics/gwas/NAM/FileNames.java
+++ src/net/maizegenetics/gwas/NAM/FileNames.java
@@ -15,20 +15,20 @@ public class FileNames {
 	public File phenotypes = null;
 	public File projectedFile = null;
 	
-	public double enterlimit = Double.NaN;
-	public double exitlimit = Double.NaN;
+	public double enterlimit = 1e-6;
+	public double exitlimit = 2e-6;
 	public double[] limits = null;
 	public int traitnumber = -1;
-	public int iterations = 0;
+	public int iterations = 100;
 	public String replacement;
 	public String analysis = "";
-	public int maxsnps = 100;
+	public int maxsnps = 1000;
 	public boolean threaded = false;
 	public boolean permute = false;
 	public boolean bootstrapPermutation = false;
 	public boolean randomizeSnpOrder = false;
 	public boolean fullModel = false;
-	public int startIteration = 0;
+	public int startIteration = 1;
 	public int chromosome = 0;
 	
 	
