cf9f979d980dc94b90cd28e22501e2a39fdb0330
Terry Casstevens <tmc46@cornell.edu>
2013-04-14 22:12:14 -0400

    Formatting Only

--- src/net/maizegenetics/gbs/maps/TOPMInterface.java
+++ src/net/maizegenetics/gbs/maps/TOPMInterface.java
@@ -1,6 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * TOPMInterface
  */
 package net.maizegenetics.gbs.maps;
 
@@ -12,68 +11,67 @@ import net.maizegenetics.pal.alignment.Locus;
  */
 public interface TOPMInterface {
 
-    int addVariant(int tagIndex, byte offset, byte base);
+    public int addVariant(int tagIndex, byte offset, byte base);
 
-    int compare(int index1, int index2);
+    public int compare(int index1, int index2);
 
-    int getChromosome(int index);
-
-    /**
-     * Returns an array whose <i>values</i> are the distinct chromosomes
-     * in this file, as stored in the chromosome[] array.  The indices are arbitrary.
-     */
-    int[] getChromosomes();
-
-    byte getDcoP(int index);
-
-    byte getDivergence(int index);
-
-    int getEndPosition(int index);
-
-    Locus[] getLoci();
-
-    Locus getLocus(int tagIndex);
-
-    byte getMapP(int index);
-
-    byte getMultiMaps(int index);
-
-    int[] getPositionArray(int index);
-
-    int getReadIndexForPositionIndex(int posIndex);
-
-    int getSize();
-
-    int getStartPosition(int index);
-
-    byte getStrand(int tagIndex);
-
-    byte getVariantDef(int tagIndex, int variantIndex);
+    public int getChromosome(int index);
 
     /**
-     * Returns an array containing all variant definitions for the
-     * tag at the supplied index.
+     * Returns an array whose <i>values</i> are the distinct chromosomes in this
+     * file, as stored in the chromosome[] array. The indices are arbitrary.
      */
-    byte[] getVariantDefArray(int tagIndex);
+    public int[] getChromosomes();
 
-    byte getVariantPosOff(int tagIndex, int variantIndex);
+    public byte getDcoP(int index);
+
+    public byte getDivergence(int index);
+
+    public int getEndPosition(int index);
+
+    public Locus[] getLoci();
+
+    public Locus getLocus(int tagIndex);
+
+    public byte getMapP(int index);
+
+    public byte getMultiMaps(int index);
+
+    public int[] getPositionArray(int index);
+
+    public int getReadIndexForPositionIndex(int posIndex);
+
+    public int getSize();
+
+    public int getStartPosition(int index);
+
+    public byte getStrand(int tagIndex);
+
+    public byte getVariantDef(int tagIndex, int variantIndex);
 
     /**
-     * Returns an array containing all variant position offsets for the
-     * tag at the supplied index.
+     * Returns an array containing all variant definitions for the tag at the
+     * supplied index.
      */
-    byte[] getVariantPosOffArray(int tagIndex);
+    public byte[] getVariantDefArray(int tagIndex);
 
-    void setChromoPosition(int index, int chromosome, byte strand, int positionMin, int positionMax);
+    public byte getVariantPosOff(int tagIndex, int variantIndex);
 
-    void setDivergence(int index, byte divergence);
+    /**
+     * Returns an array containing all variant position offsets for the tag at
+     * the supplied index.
+     */
+    public byte[] getVariantPosOffArray(int tagIndex);
 
-    void setMapP(int index, byte mapP);
+    public void setChromoPosition(int index, int chromosome, byte strand, int positionMin, int positionMax);
 
-    void setMapP(int index, double mapP);
+    public void setDivergence(int index, byte divergence);
 
-    void setVariantDef(int tagIndex, int variantIndex, byte def);
+    public void setMapP(int index, byte mapP);
 
-    void setVariantPosOff(int tagIndex, int variantIndex, byte offset);
-    
+    public void setMapP(int index, double mapP);
+
+    public void setVariantDef(int tagIndex, int variantIndex, byte def);
+
+    public void setVariantPosOff(int tagIndex, int variantIndex, byte offset);
 }
