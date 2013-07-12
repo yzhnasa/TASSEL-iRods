a2ccdafc735aa1f0d277a36b9bf9cdf0102fb342
Edward Buckler <esb33@cornell.edu>
2013-07-12 12:44:32 -0400

    Add annotatedID multi map based on any key

--- src/net/maizegenetics/pal/ids/IdGroupIOUtils.java
+++ src/net/maizegenetics/pal/ids/IdGroupIOUtils.java
@@ -20,8 +20,25 @@ public class IdGroupIOUtils {
         for (int i = 0; i < annoIdGroup.getIdCount(); i++) {
             if(annoIdGroup.getIdentifier(i) instanceof AnnotatedIdentifier) {
                 AnnotatedIdentifier ai=(AnnotatedIdentifier)annoIdGroup.getIdentifier(i);
-                for (String value : ai.getTextAnnotation(annoName)) {
-                    annoMap.put(value,ai.getFullName());
+                for (String key : ai.getTextAnnotation(annoName)) {
+                    annoMap.put(key,ai.getFullName());
+                } 
+            }
+        }
+        return annoMap;
+    }
+    
+    public static TreeMultimap<String,String> getMapOfTextAnnotatedIds(IdGroup annoIdGroup, String keyAnnoName, String valueAnnoName) {
+        TreeMultimap<String,String> annoMap=TreeMultimap.create();
+        for (int i = 0; i < annoIdGroup.getIdCount(); i++) {
+            if(annoIdGroup.getIdentifier(i) instanceof AnnotatedIdentifier) {
+                AnnotatedIdentifier ai=(AnnotatedIdentifier)annoIdGroup.getIdentifier(i);
+                for (String key : ai.getTextAnnotation(keyAnnoName)) {
+                    String[] values=ai.getTextAnnotation(valueAnnoName);
+                    for (String value : values) {
+                        annoMap.put(key,value);
+                    }
+                    
                 } 
             }
         }
