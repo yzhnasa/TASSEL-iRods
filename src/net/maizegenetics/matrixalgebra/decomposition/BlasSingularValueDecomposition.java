379dd9696eba7b2c4098eaf4b9efc026b262417a
Peter Bradbury <pjb39@cornell.edu>
2013-07-15 17:35:28 -0400

    add code to implement jobz types

--- src/net/maizegenetics/matrixalgebra/decomposition/BlasSingularValueDecomposition.java
+++ src/net/maizegenetics/matrixalgebra/decomposition/BlasSingularValueDecomposition.java
@@ -1,14 +1,13 @@
 package net.maizegenetics.matrixalgebra.decomposition;
 
+import java.util.Arrays;
+
 import org.apache.log4j.Logger;
 
 import net.maizegenetics.matrixalgebra.Matrix.BlasDoubleMatrix;
 import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrix;
 
-public class BlasSingularValueDecomposition implements
-		SingularValueDecomposition {
-//	use 	public static native int singularValueDecomposition(char jobu, char jobvt, int nrows, int ncols, int ns, double[] A, double[] S, double[] U, double[] VT);
-//	in BlasDoubleMatrix
+public class BlasSingularValueDecomposition implements SingularValueDecomposition {
 	private BlasDoubleMatrix bdS = null;
 	private BlasDoubleMatrix bdU = null;
 	private BlasDoubleMatrix bdVT = null;
@@ -20,22 +19,65 @@ public class BlasSingularValueDecomposition implements
 		int nrows = bdm.numberOfRows();
 		int ncols = bdm.numberOfColumns();
 		int ns = Math.min(nrows, ncols);
+		double[] A = bdm.getMatrixCopy();
 		double[] S = new double[ns];
-		int urows = nrows;
-		int ucols = ns;
-		int usize = urows * ucols;
-		double[] U = new double[usize];
-		int vtrows = ns;
-		int vtcols = ncols;
-		int vtsize = vtrows * vtcols;
-		double[] VT = new double[vtsize];
-
-		int result = BlasDoubleMatrix.singularValueDecompositionDgesdd(jobz, nrows, ncols, bdm.getMatrix(), nrows, S, U, nrows, VT, ncols);
+		int usize = nrows * nrows;
+		
+		double[] U;
+		if (jobz == 'N' || (jobz == 'O'& nrows >= ncols)) {
+			U = new double[]{0};
+		} else {
+			U = new double[usize];
+		}
+		
+		double[] VT;
+		int vtsize = ncols * ncols;
+		if (jobz == 'N' || (jobz == 'O'& nrows < ncols)) {
+			VT = new double[]{0};
+		} else {
+			VT = new double[vtsize];
+		}
+		
+		int result = BlasDoubleMatrix.singularValueDecompositionDgesdd(jobz, nrows, ncols, A, nrows, S, U, nrows, VT, ncols);
 		if (result == 0) {
 			successful = true;
 			bdS = new BlasDoubleMatrix(ns, 1, S, true);
-			bdU = new BlasDoubleMatrix(urows, ucols, U, true);
-			bdVT = new BlasDoubleMatrix(vtrows, vtcols, VT, true);
+			if (jobz == 'A') {
+				bdU = new BlasDoubleMatrix(nrows, nrows, U, true);
+				bdVT = new BlasDoubleMatrix(ncols, ncols, VT, true);
+			} else if (jobz == 'S') {
+				if (nrows < ncols) {
+					bdU = new BlasDoubleMatrix(nrows, nrows, U, true);
+					int[] selection = new int[nrows];
+					for (int i = 0; i < nrows; i++) selection[i] = i;
+					double[] subvt = BlasDoubleMatrix.getSelectionFromDoubleArray(VT, ncols, ncols, selection, null);
+					bdVT = new BlasDoubleMatrix(nrows, ncols, subvt, true);
+				} else if (nrows > ncols) {
+					int[] selection = new int[ncols];
+					for (int i = 0; i < ncols; i++) selection[i] = i;
+					double[] subu = BlasDoubleMatrix.getSelectionFromDoubleArray(U, nrows, nrows, null, selection);
+					bdU = new BlasDoubleMatrix(nrows, ncols, subu, true);
+					bdVT = new BlasDoubleMatrix(ncols, ncols, VT, true);
+				} else {
+					bdU = new BlasDoubleMatrix(nrows, nrows, U, true);
+					bdVT = new BlasDoubleMatrix(ncols, ncols, VT, true);
+				}
+			} else if (jobz == 'O') {
+				if (nrows >= ncols) {
+					bdVT = new BlasDoubleMatrix(ncols, ncols, VT, true);
+					int[] selection = new int[ncols];
+					for (int i = 0; i < ncols; i++) selection[i] = i;
+					U = BlasDoubleMatrix.getSelectionFromDoubleArray(A, nrows, ncols, null, selection);
+					bdU = new BlasDoubleMatrix(nrows, ncols, U, true);
+				} else {
+					bdU = new BlasDoubleMatrix(nrows, nrows, U, true);
+					int[] selection = new int[nrows];
+					for (int i = 0; i < nrows; i++) selection[i] = i;
+					VT = BlasDoubleMatrix.getSelectionFromDoubleArray(A, nrows, ncols, selection, null);
+					bdVT = new BlasDoubleMatrix(nrows, ncols, VT, true);
+				}
+			}
+			
 		} else {
 			myLogger.error("BlasSVD failed with a return value of " + result);
 		}
@@ -78,4 +120,6 @@ public class BlasSingularValueDecomposition implements
 	public double getTolerance() { return tol; }
 	
 	public void setTolerance(double tolerance) { tol = tolerance; }
+	
+	public boolean wasSuccessful() { return successful; }
 }
