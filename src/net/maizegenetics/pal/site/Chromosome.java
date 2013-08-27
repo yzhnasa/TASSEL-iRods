921840ae581a7f4d3410149be07f31b6105d16ef
Terry Casstevens <tcasstevens@gmail.com>
2013-08-27 17:58:22 -0400

    Formatting only

--- src/net/maizegenetics/pal/site/Chromosome.java
+++ src/net/maizegenetics/pal/site/Chromosome.java
@@ -2,14 +2,10 @@ package net.maizegenetics.pal.site;
 
 import net.maizegenetics.pal.alignment.Locus;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Defines the chromosome structure and length.  The name and length recorded for 
- * each chromosome. 
- * 
+ * Defines the chromosome structure and length. The name and length recorded for
+ * each chromosome.
+ *
  * @author Terry Casstevens and Ed Buckler
  */
 public class Chromosome implements Comparable<Chromosome>, GeneralAnnotation {
@@ -24,21 +20,23 @@ public class Chromosome implements Comparable<Chromosome>, GeneralAnnotation {
 
     /**
      *
-     * @param name Name of the chromosome 
+     * @param name Name of the chromosome
      * @param length Length of chromosome in base pairs
      * @param features Map of features about the chromosome
      */
     public Chromosome(String name, int length, GeneralAnnotation features) {
         myName = name;
         myLength = length;
-        int convChr=Integer.MAX_VALUE;
-        try{convChr=Integer.parseInt(name);}
-        catch(NumberFormatException ne) {}
-        myChromosomeNumber=convChr;
-        myGA=features;
-        hashCode=calcHashCode();
+        int convChr = Integer.MAX_VALUE;
+        try {
+            convChr = Integer.parseInt(name);
+        } catch (NumberFormatException ne) {
+        }
+        myChromosomeNumber = convChr;
+        myGA = features;
+        hashCode = calcHashCode();
     }
-    
+
     public Chromosome(String name) {
         this(name, -1, null);
     }
@@ -46,10 +44,10 @@ public class Chromosome implements Comparable<Chromosome>, GeneralAnnotation {
     public String getName() {
         return myName;
     }
-    
+
     /**
-     * Returns the interger value of the chromosome (if name is not a number then
-     * Integer.MAX_VALUE is returned)
+     * Returns the interger value of the chromosome (if name is not a number
+     * then Integer.MAX_VALUE is returned)
      */
     public int getChromosomeNumber() {
         return myChromosomeNumber;
@@ -73,7 +71,6 @@ public class Chromosome implements Comparable<Chromosome>, GeneralAnnotation {
     public double[] getQuantAnnotation(String annoName) {
         return myGA.getQuantAnnotation(annoName);
     }
-
 
     @Override
     public String getConsensusAnnotation(String annoName) {
@@ -100,19 +97,25 @@ public class Chromosome implements Comparable<Chromosome>, GeneralAnnotation {
         hash = 79 * hash + (this.myName != null ? this.myName.hashCode() : 0);
         return hash;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
-        if (obj == this) {return true;}
-        if (!(obj instanceof Chromosome)) {return false;}
-        return (compareTo((Chromosome)obj)==0);
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Chromosome)) {
+            return false;
+        }
+        return (compareTo((Chromosome) obj) == 0);
     }
 
     @Override
     public int compareTo(Chromosome o) {
         //int result=Integer.compare(myChromosomeNumber,o.getChromosomeNumber());
-        int result=Integer.valueOf(myChromosomeNumber).compareTo(Integer.valueOf(o.getChromosomeNumber()));
-        if(result!=0) return result;
+        int result = Integer.valueOf(myChromosomeNumber).compareTo(Integer.valueOf(o.getChromosomeNumber()));
+        if (result != 0) {
+            return result;
+        }
         return myName.compareTo(o.getName());
     }
 }
