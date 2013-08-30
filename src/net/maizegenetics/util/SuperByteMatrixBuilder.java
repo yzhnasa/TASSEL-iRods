3e6cae00f154f811f3f49d1b7db398c08e1dd8ff
Terry Casstevens <tcasstevens@gmail.com>
2013-08-29 23:48:53 -0400

    Added factory method to transpose existing SuperByteMatrix.

--- src/net/maizegenetics/util/SuperByteMatrixBuilder.java
+++ src/net/maizegenetics/util/SuperByteMatrixBuilder.java
@@ -23,4 +23,25 @@ public class SuperByteMatrixBuilder {
     public static SuperByteMatrix getInstanceTranspose(int rows, int columns) {
         return new SuperByteMatrixTranspose(rows, columns);
     }
+
+    public static SuperByteMatrix getInstanceTranspose(SuperByteMatrix matrix) {
+
+        int numRows = matrix.getNumRows();
+        int numColumns = matrix.getNumColumns();
+        SuperByteMatrix result;
+
+        if (matrix instanceof SuperByteMatrixSingle) {
+            result = getInstanceTranspose(numRows, numColumns);
+        } else if (matrix instanceof SuperByteMatrixTranspose) {
+            result = getInstance(numRows, numColumns);
+        } else {
+            throw new IllegalArgumentException("SuperByteMatrixBuilder: getInstanceTranspose: Don't Know how to Transpose: " + matrix.getClass().getName());
+        }
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numColumns; c++) {
+                result.set(r, c, matrix.get(r, c));
+            }
+        }
+        return result;
+    }
 }
