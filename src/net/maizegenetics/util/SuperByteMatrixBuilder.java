246fe73b436f73f350011daf389c8c9591f49c88
Terry Casstevens <tcasstevens@gmail.com>
2013-09-03 18:18:40 -0400

    Faster implementation to transpose between TByte and SByte.  seeing about 12x faster for SuperByteMatrixMultiple and about 7x faster for SuperByteMatrixSingle.

--- src/net/maizegenetics/util/SuperByteMatrixBuilder.java
+++ src/net/maizegenetics/util/SuperByteMatrixBuilder.java
@@ -26,22 +26,69 @@ public class SuperByteMatrixBuilder {
 
     public static SuperByteMatrix getInstanceTranspose(SuperByteMatrix matrix) {
 
+        int blockSize = 64;
         int numRows = matrix.getNumRows();
         int numColumns = matrix.getNumColumns();
         SuperByteMatrix result;
 
-        if (matrix instanceof SuperByteMatrixSingle) {
+        if ((matrix instanceof SuperByteMatrixSingle) || (matrix instanceof SuperByteMatrixMultiple)) {
             result = getInstanceTranspose(numRows, numColumns);
+            int currentCSize = blockSize;
+            byte[][] temp = new byte[blockSize][blockSize];
+            for (int bigC = 0; bigC < numColumns; bigC += blockSize) {
+                if (numColumns - bigC < blockSize) {
+                    currentCSize = numColumns - bigC;
+                }
+                int currentRSize = blockSize;
+                for (int bigR = 0; bigR < numRows; bigR += blockSize) {
+                    if (numRows - bigR < blockSize) {
+                        currentRSize = numRows - bigR;
+                    }
+
+                    for (int r = 0; r < currentRSize; r++) {
+                        for (int c = 0; c < currentCSize; c++) {
+                            temp[r][c] = matrix.get(r + bigR, c + bigC);
+                        }
+                    }
+
+                    for (int c = 0; c < currentCSize; c++) {
+                        for (int r = 0; r < currentRSize; r++) {
+                            result.set(r + bigR, c + bigC, temp[r][c]);
+                        }
+                    }
+                }
+            }
         } else if (matrix instanceof SuperByteMatrixTranspose) {
             result = getInstance(numRows, numColumns);
+            int currentCSize = blockSize;
+            byte[][] temp = new byte[blockSize][blockSize];
+            for (int bigC = 0; bigC < numColumns; bigC += blockSize) {
+                if (numColumns - bigC < blockSize) {
+                    currentCSize = numColumns - bigC;
+                }
+                int currentRSize = blockSize;
+                for (int bigR = 0; bigR < numRows; bigR += blockSize) {
+                    if (numRows - bigR < blockSize) {
+                        currentRSize = numRows - bigR;
+                    }
+
+                    for (int c = 0; c < currentCSize; c++) {
+                        for (int r = 0; r < currentRSize; r++) {
+                            temp[r][c] = matrix.get(r + bigR, c + bigC);
+                        }
+                    }
+
+                    for (int r = 0; r < currentRSize; r++) {
+                        for (int c = 0; c < currentCSize; c++) {
+                            result.set(r + bigR, c + bigC, temp[r][c]);
+                        }
+                    }
+                }
+            }
         } else {
             throw new IllegalArgumentException("SuperByteMatrixBuilder: getInstanceTranspose: Don't Know how to Transpose: " + matrix.getClass().getName());
         }
-        for (int r = 0; r < numRows; r++) {
-            for (int c = 0; c < numColumns; c++) {
-                result.set(r, c, matrix.get(r, c));
-            }
-        }
+
         return result;
     }
 }
