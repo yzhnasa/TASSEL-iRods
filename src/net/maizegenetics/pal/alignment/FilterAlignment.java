3bab395fe68ab0ec6a5c24754ae7789344586874
Terry Casstevens <tcasstevens@gmail.com>
2013-09-30 16:57:12 -0400

    Fixed FilterAlignment.getMajorMinorCounts()

--- src/net/maizegenetics/pal/alignment/FilterAlignment.java
+++ src/net/maizegenetics/pal/alignment/FilterAlignment.java
@@ -1399,21 +1399,14 @@ public class FilterAlignment implements Alignment {
         int numSites = getSiteCount();
         long[][] counts = new long[16][16];
 
-        if (getMaxNumAlleles() >= 2) {
-            for (int site = 0; site < numSites; site++) {
-                byte[] alleles = getAlleles(site);
-                byte indexI = alleles[0];
-                byte indexJ = alleles[1];
-                if (indexJ == UNKNOWN_ALLELE) {
-                    indexJ = indexI;
-                }
-                counts[indexI][indexJ]++;
-            }
-        } else {
-            for (int site = 0; site < numSites; site++) {
-                byte[] alleles = getAlleles(site);
-                byte indexI = alleles[0];
-                counts[indexI][indexI]++;
+        for (int site = 0; site < numSites; site++) {
+            byte[] alleles = getAlleles(site);
+            if ((alleles == null) || alleles.length == 0) {
+                // do nothing
+            } else if (alleles.length == 1) {
+                counts[alleles[0]][alleles[0]]++;
+            } else {
+                counts[alleles[0]][alleles[1]]++;
             }
         }
 
