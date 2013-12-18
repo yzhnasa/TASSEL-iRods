1b4fceb54b8a5ddcaaff1cc5ef9bbb9298febdd9
Terry Casstevens <tcasstevens@gmail.com>
2013-12-18 13:35:51 -0500

    Implemented AlleleDepthUtil.depthIntToByte().  Still need the other direction.

--- src/net/maizegenetics/dna/snp/depth/AlleleDepthUtil.java
+++ src/net/maizegenetics/dna/snp/depth/AlleleDepthUtil.java
@@ -6,20 +6,26 @@ package net.maizegenetics.dna.snp.depth;
 /**
  *
  * @author Terry Casstevens
+ * @author Robert Bukowski
  */
 public class AlleleDepthUtil {
+
+    private static double LOG_BASE = 1.0746;  // logbase^128 = 10,000
+    private static double LOG_CONV = 1.0 / Math.log(LOG_BASE);
 
     private AlleleDepthUtil() {
         // utility
     }
 
     public static byte depthIntToByte(int depth) {
-        if (depth < 128) {
-            return (byte) depth;
-        } else {
-            // TODO: Need calculation for higher depths
-            return (byte) 127;
+        if (depth > 127) {
+            depth = (int) (-LOG_CONV * Math.log(depth));
+            if (depth < -128) {
+                depth = -127;
+            }
         }
+
+        return (byte) depth;
     }
 
     public static int depthByteToInt(byte depth) {
