6faa8819fa540034b8c89bd24806c31e53467cb3
Terry Casstevens <tcasstevens@gmail.com>
2013-12-20 17:35:01 -0500

    Added new int to byte decoding algorithm for Allele Depths designed by Robert B.

--- src/net/maizegenetics/dna/snp/depth/AlleleDepthUtil.java
+++ src/net/maizegenetics/dna/snp/depth/AlleleDepthUtil.java
@@ -12,10 +12,14 @@ import java.util.Arrays;
  */
 public class AlleleDepthUtil {
 
-    private static final double LOG_BASE = 1.0746;  // LOG_BASE^128 = 10,356
-    private static final double LOG_CONV = 1.0 / Math.log(LOG_BASE);
-    private static final double DECODE_LOG_CONV = Math.log(LOG_BASE);
+    private static final double LOG_BASE = 1.0746;  // LOG_BASE^128 = 10,482
+    private static final double R_LOG_CONV = 1.0 / Math.log(LOG_BASE);
+    private static final double LOG_CONV = 1.0 / R_LOG_CONV;
     private static final int[] BYTE_TO_INT = new int[256];
+    private static final int MAX_ACC_DEPTH = 182;
+    private static final int MIN_ACC_BYTE = 127 - MAX_ACC_DEPTH;
+    private static final int OFFSET = 126;
+    private static final double ADJ = 0.5;
 
     static {
         Arrays.fill(BYTE_TO_INT, -1);
@@ -29,26 +33,39 @@ public class AlleleDepthUtil {
     }
 
     public static byte depthIntToByte(int depth) {
-        if (depth > 127) {
-            depth = (int) (-LOG_CONV * Math.log(depth));
-            if (depth < -128) {
-                depth = -128;
+        byte bdepth;
+        int itd;
+
+        if (depth <= 127) {
+            itd = depth;
+        } else if (depth <= MAX_ACC_DEPTH) {
+            itd = 127 - depth;
+        } else {
+            itd = (int) (-R_LOG_CONV * Math.log(depth - OFFSET));
+            if (itd < -128) {
+                itd = -128;
             }
         }
+        bdepth = (byte) itd;
 
-        return (byte) depth;
+        return bdepth;
     }
 
     public static int depthByteToInt(byte depth) {
         return BYTE_TO_INT[depth & 0xFF];
     }
 
-    private static int decode(byte depth) {
-        if (depth >= 0) {
-            return depth;
+    private static int decode(byte bdepth) {
+        int depth;
+        if (bdepth >= 0) {
+            depth = bdepth;
+        } else if (bdepth >= MIN_ACC_BYTE) {
+            depth = 127 - bdepth;
         } else {
-            return (int) (Math.exp(-DECODE_LOG_CONV * (depth - 0.5)));
+            depth = OFFSET + (int) (Math.exp(-LOG_CONV * (bdepth - ADJ)));
         }
+
+        return depth;
     }
 
 }
