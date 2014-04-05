d63e9aa8c73f5b652a693f8271faacf1331a3c24
Terry Casstevens <tcasstevens@gmail.com>
2014-04-05 18:41:28 -0400

    Updated package-info.java for net.maizegenetics.pipeline Task #TAS-298

--- src/net/maizegenetics/pipeline/package-info.java
+++ src/net/maizegenetics/pipeline/package-info.java
@@ -1,5 +1,7 @@
 /**
- * Runs TASSEL pipelines built with a set of plugins.
+ * Runs TASSEL Pipeline (Command Line Interface) built with a set of plugins.
+ * <p/>
+ * <a href="http://www.maizegenetics.net/tassel/docs/TasselPipelineCLI.pdf">http://www.maizegenetics.net/tassel/docs/TasselPipelineCLI.pdf</a> 
  * @author Terry Casstevens
  */
 package net.maizegenetics.pipeline;
